bb7feef72057e877b376a277f132a63b6771a08f
sm-nazmus-sakib <nazmussakibnwu@gmal.com>
2022-10-28 16:02:47 +0600

    blog added

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Blog = () => {
-  return <div>This is Blog Site</div>;
+  return <div className="p-4">
+    <div>
+      <h5> #What is cors?</h5>
+      <p>In ReactJS, Cross-Origin Resource Sharing (CORS) refers to the method that allows you to make requests to the server deployed at a different domain. As a reference, if the frontend and backend are at two different domains, we need CORS there. The CORS mechanism supports secure cross-origin requests and data transfers between browsers and servers. Modern browsers use CORS in APIs such as XMLHttpRequest or Fetch to mitigate the risks of cross-origin HTTP requests.</p>
+      <h5> #Why we use firebase? What other options do you have to implement authentication?</h5>
+      <p>The Firebase Realtime Database lets you build rich, collaborative applications by allowing secure access to the database directly from client-side code. Data is persisted locally, and even while offline, realtime events continue to fire, giving the end user a responsive experience. <br /> Auth0, MongoDB, Passport, Okta, and Firebase are the most popular alternatives and competitors to Firebase Authentication.</p>
+      <h5>#How does the private route work?</h5>
+      <p>The react private route component renders child components ( children ) if the user is logged in. If not logged in the user is redirected to the /login page with the return url passed in the location state property</p>
+      <h5>What is Node? How does Node work?</h5>
+      <p>Node.js is an open-source, cross-platform, back-end JavaScript runtime environment that runs on a JavaScript Engine and executes JavaScript code outside a web browser, which was designed to build scalable network applications. <br /> It is a used as backend service where javascript works on the server-side of the application. This way javascript is used on both frontend and backend. Node. js runs on chrome v8 engine which converts javascript code into machine code, it is highly scalable, lightweight, fast, and data-intensive.</p>
+    </div>
+  </div>;
 };
 
 export default Blog;
